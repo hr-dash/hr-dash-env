2ce5a8a4c15451dd079537c610aaadce9f8bda8f
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-06-05 00:02:59 +0900

    Use node for user attributes

--- cookbooks/user/default.rb
+++ cookbooks/user/default.rb
@@ -1,7 +1,11 @@
-user 'web' do
+name = node[:user][:name]
+pass = node[:user][:password]
+salt = node[:user][:salt]
+
+user name do
   action :create
   user 'root'
-  password 'password'
   system_user true
+  password pass.crypt(salt)
   create_home true
 end
