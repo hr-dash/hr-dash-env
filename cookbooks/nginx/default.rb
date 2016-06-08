4332657aeb2b2e0f4c541d3153cee04c560e70ab
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-06-09 02:28:45 +0900

    Add nginx repo before package

--- cookbooks/nginx/default.rb
+++ cookbooks/nginx/default.rb
@@ -1,3 +1,8 @@
+execute 'add repo' do
+  command 'rpm -ivh http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm'
+  not_if 'rpm -q nginx-release-centos-6-0.el6.ngx'
+end
+
 package 'nginx' do
   action :install
 end
