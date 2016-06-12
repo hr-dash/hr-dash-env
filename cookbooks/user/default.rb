0b7eb083109d94684aabd6adc6584239b2326f4e
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-06-12 14:18:58 +0900

    Add web user to wheel group

--- cookbooks/user/default.rb
+++ cookbooks/user/default.rb
@@ -1,6 +1,7 @@
 name = node[:user][:name]
 pass = node[:user][:password]
 salt = node[:user][:salt]
+wheel_gid = '10'
 
 user name do
   action :create
@@ -8,4 +9,8 @@ user name do
   system_user true
   password pass.crypt(salt)
   create_home true
+end
+
+user name do
+  gid wheel_gid
 end
