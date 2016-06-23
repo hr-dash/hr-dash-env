11f20c712054e84b2feddfba11172762b5afae66
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-06-24 07:41:21 +0900

    Control pg_hba.conf and postgres user, password in recipe

--- cookbooks/postgres/default.rb
+++ cookbooks/postgres/default.rb
@@ -1,3 +1,11 @@
+def restart_postgres(ver)
+  [:enable, :restart].each do |act|
+    service "postgresql-#{ver}" do
+      action act
+    end
+  end
+end
+
 # PostgreSQL Install
 ver = node[:postgresql][:ver]
 short_ver = node[:postgresql][:short_ver]
@@ -17,8 +25,29 @@ execute "initdb" do
   command "service postgresql-#{ver} initdb"
 end
 
-[:enable, :restart].each do |act|
-  service "postgresql-#{ver}" do
-    action act
-  end
+#####################
+# Set Password
+#####################
+template "/var/lib/pgsql/#{ver}/data/pg_hba.conf" do
+  owner 'postgres'
+  group 'postgres'
+  variables(auth_method: :trust)
 end
+restart_postgres(ver)
+
+execute "set password" do
+  user = node[:postgresql][:user]
+  password = node[:postgresql][:password]
+  command "psql -U #{user} -c \"ALTER ROLE #{user} WITH PASSWORD '#{password}';\""
+end
+
+#####################
+# Reset pg_hba.conf
+#####################
+template "/var/lib/pgsql/#{ver}/data/pg_hba.conf" do
+  owner 'postgres'
+  group 'postgres'
+  variables(auth_method: :md5)
+end
+restart_postgres(ver)
+
